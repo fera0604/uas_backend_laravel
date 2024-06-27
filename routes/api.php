233fbf5f3bdc9a4b1e8a63bab3889c0b1ba47003
fera0604 <f3ra0604@gmail.com>
2024-06-27 11:44:57 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
+    Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth:api');
+});
